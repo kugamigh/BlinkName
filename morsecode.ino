972c4f64e5eb55fef935ec4cded3e6b60a0473c3
kugamigh <48243143+kugamigh@users.noreply.github.com>
2019-03-17 15:48:27 +1100

    Update morsecode.ino

--- morsecode.ino
+++ morsecode.ino
@@ -10,71 +10,74 @@ void loop() {
         If the duration of a dot is taken to be one unit then that of a dash is three units. 
         The space between the components of one character is one unit, between characters is three units and between words seven units.
         
-        One unit = 500ms
+        One unit = 100ms
     */
     
     // The letter J is .---
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(100);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(100);
     digitalWrite(LED, HIGH);
-    delay(1500);
+    delay(300);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(100);
     digitalWrite(LED, HIGH);
-    delay(1500);
+    delay(300);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(1500);
+    delay(300);
     digitalWrite(LED, LOW);
-    delay(1500);
+    delay(300);
     
     // The letter o is ---
     digitalWrite(LED, HIGH);
-    delay(1500);
+    delay(300);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(1500);
+    delay(300);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(1500);
+    delay(300);
     digitalWrite(LED, LOW);
-    delay(1500);    
+    delay(300);
+    
     
     // The letter s is ...
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(1500);
+    delay(300);
+    
     
     // The letter h is ....
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(500);
+    delay(150);
     digitalWrite(LED, HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(LED, LOW);
-    delay(1500);
+    delay(700);
+    
     
 }
